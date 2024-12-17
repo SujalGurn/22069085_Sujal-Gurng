98dd16970da41ce22b8469fdb702364ca7604c4f
Sujal Gurung <sujalgurn@gmail.com>
2024-12-17 20:52:21 +0545

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpensesManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpensesManager
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
